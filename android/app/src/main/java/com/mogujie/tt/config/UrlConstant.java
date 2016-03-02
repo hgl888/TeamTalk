bcdb5fec623af4f9937a0144ffdb94fa1a28d783
hgl888 <hgl868@126.com>
2016-03-02 18:04:39 +0800

    修msg_address url

--- android/app/src/main/java/com/mogujie/tt/config/UrlConstant.java
+++ android/app/src/main/java/com/mogujie/tt/config/UrlConstant.java
@@ -11,6 +11,7 @@ public class UrlConstant {
     public final static String AVATAR_URL_PREFIX = "";
 
     // access 地址
-    public final static String ACCESS_MSG_ADDRESS = "http://61.153.100.221:8080/msg_server";
+    //public final static String ACCESS_MSG_ADDRESS = "http://61.153.100.221:8080/msg_server";
+    public final static String ACCESS_MSG_ADDRESS = "http://172.28.18.86:8080/msg_server";
 
 }
