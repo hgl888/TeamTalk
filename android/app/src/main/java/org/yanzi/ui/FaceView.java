1a598bf66c345fa8d879d90341b72dd6d3f80b2d
houguoli <houguoli@syswin.com>
2016-03-22 11:25:24 +0800

    添加iOS client

--- android/app/src/main/java/org/yanzi/ui/FaceView.java
+++ android/app/src/main/java/org/yanzi/ui/FaceView.java
@@ -5,17 +5,20 @@ import org.yanzi.util.Util;
 
 import com.mogujie.tt.R;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.PixelFormat;
 import android.graphics.PointF;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.Face;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class FaceView extends ImageView {
@@ -27,14 +30,30 @@ public class FaceView extends ImageView {
 	private RectF mRect = new RectF();
 	private float[] mPoint = new float[3];
 	private Drawable mFaceIndicator = null;
+	private Drawable mFaceHat = null;
+	private Bitmap mBtmHat = null;
 	public FaceView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
 		initPaint();
 		mContext = context;
 		mFaceIndicator = getResources().getDrawable(R.drawable.ic_face_find);
+		mFaceHat = getResources().getDrawable(R.drawable.ic_hat );
+		drawableToBitmap( mFaceHat );
 	}
 
+	Bitmap drawableToBitmap(Drawable drawable) // drawable 转换成 bitmap
+	{
+		int width = drawable.getIntrinsicWidth();   // 取 drawable 的长宽
+		int height = drawable.getIntrinsicHeight();
+		Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888:Bitmap.Config.RGB_565;         // 取 drawable 的颜色格式
+		mBtmHat = Bitmap.createBitmap(width, height, config);     // 建立对应 bitmap
+		Canvas canvas = new Canvas(mBtmHat);         // 建立对应 bitmap 的画布
+		drawable.setBounds(0, 0, width, height);
+		drawable.draw(canvas);      // 把 drawable 内容画到画布中
+		canvas = null;
+		return mBtmHat;
+	}
 
 	public void setFaces(Face[] faces){
 		this.mFaces = faces;
@@ -69,17 +88,36 @@ public class FaceView extends ImageView {
             //mFaceIndicator.setBounds(Math.round(mRect.left), Math.round(mRect.top),
             //        Math.round(mRect.right), Math.round(mRect.bottom));
             //mFaceIndicator.draw(canvas);
-			canvas.drawRect(mRect, mLinePaint);
+			//canvas.drawRect(mRect, mLinePaint);
 			//mPoint[0] = mFaces[i].rightEye.x;
 			//mPoint[1] = mFaces[i].rightEye.y;
 			//mMatrix.mapPoints(mPoint);
-			canvas.drawCircle( mRect.centerX(), mRect.centerY(), 10, mLinePaint);
+			RectF dst = new RectF();
+			float frate = (float)(1.2 * mRect.width()) /(float)mFaceHat.getMinimumWidth();
+			dst.left = mRect.centerX() - mRect.width();
+			dst.right = mRect.centerX() + mRect.width();
+			dst.top = mRect.top - frate * mFaceHat.getMinimumHeight();
+			dst.bottom = mRect.top;
+
+//			//Log.e("");
+//			Log.e("FaceView", "l=" + left + ";r=" + right + ";t=" + top + ";b=" + bottom );
+//
+//			mFaceHat.setBounds( Math.round(left),
+//					Math.round(right),
+//					Math.round(top),
+//					Math.round(bottom));
+
+			//mFaceHat.draw( canvas );
+
+
+			canvas.drawBitmap(mBtmHat, null, dst ,mLinePaint );
+			canvas.drawCircle(mRect.centerX(), mRect.centerY(), 10, mLinePaint);
 			canvas.drawCircle( mRect.left + mRect.width() / 4,
 					mRect.top + mRect.height() / 4, 10, mLinePaint );
 			canvas.drawCircle( mRect.right - mRect.width()/ 4,
 					mRect.top + mRect.height() / 4, 10, mLinePaint );
-			canvas.drawCircle( mRect.centerX(), mRect.bottom - mRect.height() / 4,
-					10, mLinePaint );
+			//canvas.drawCircle( mRect.centerX(), mRect.bottom - mRect.height() / 4,
+			//		10, mLinePaint );
 			return;
 		}
 		canvas.restore();
@@ -94,6 +132,7 @@ public class FaceView extends ImageView {
 		mLinePaint.setColor(color);
 		mLinePaint.setStyle(Style.STROKE);
 		mLinePaint.setStrokeWidth(5f);
-		mLinePaint.setAlpha(180);
+		//mLinePaint.setAlpha(180);
+		return;
 	}
 }
--- android/magicfilter/src/main/res/raw/fairytale_fra.glsl
+++ android/magicfilter/src/main/res/raw/fairytale_fra.glsl
@@ -1,34 +1,31 @@
 varying highp vec2 textureCoordinate;
+varying highp vec2 vposition;
 
 uniform sampler2D inputImageTexture;
 uniform sampler2D inputImageTexture2;
 
 void main()
 {
-	lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+	vec2 p = (2.0 * vposition.xy);
+	p.y -= 0.25;
+	vec3 bcol = vec3( 1.0, 0.8, 0.7 - 0.07 * p.y) * (1.0 - 0.25 * length(p));
 
-	mediump float blueColor = textureColor.b * 63.0;
+	//shape
+	float a = atan( p.x, p.y)/3.1415926;
+	float r = length( p);
+	float h = abs(a);
+	float d = (13.0 * h - 22.0 * h * h + 10.0 *h * h) / (6.0 - 5.0 * h);
 
-	mediump vec2 quad1;
-	quad1.y = floor(floor(blueColor) / 8.0);
-	quad1.x = floor(blueColor) - (quad1.y * 8.0);
 
-	mediump vec2 quad2;
-	quad2.y = floor(ceil(blueColor) / 8.0);
-	quad2.x = ceil(blueColor) - (quad2.y * 8.0);
+	//color
+	float s = 1.0-0.5*clamp(r/d,0.0,1.0);
+    s = 0.75 + 0.75*p.x;
+    s *= 1.0-0.25*r;
+    s = 0.5 + 0.6*s;
+    s *= 0.5+0.5*pow( 1.0-clamp(r/d, 0.0, 1.0 ), 0.1 );
+    vec3 hcol = vec3(1.0,0.5*r,0.3)*s;
+    vec3 col = mix( bcol, hcol, smoothstep( -0.01, 0.01, d-r) );
+    gl_FragColor = vec4(col,1.0);
 
-	highp vec2 texPos1;
-	texPos1.x = (quad1.x * 0.125) + 0.5/512.0 + ((0.125 - 1.0/512.0) * textureColor.r);
-	texPos1.y = (quad1.y * 0.125) + 0.5/512.0 + ((0.125 - 1.0/512.0) * textureColor.g);
-
-	highp vec2 texPos2;
-	texPos2.x = (quad2.x * 0.125) + 0.5/512.0 + ((0.125 - 1.0/512.0) * textureColor.r);
-	texPos2.y = (quad2.y * 0.125) + 0.5/512.0 + ((0.125 - 1.0/512.0) * textureColor.g);
-
-	lowp vec4 newColor1 = texture2D(inputImageTexture2, texPos1);
-	lowp vec4 newColor2 = texture2D(inputImageTexture2, texPos2);
-
-	lowp vec4 newColor = mix(newColor1, newColor2, fract(blueColor));
-	gl_FragColor = vec4(newColor.rgb, textureColor.w);
 
 }
